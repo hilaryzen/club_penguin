06bc62f5c92f0900067e5b5b205a11e9bdd9bb77
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-01-02 14:50:18 -0500

    some constants i just looked up, will probably be useful

---
+++ render.h
@@ -0,0 +1,9 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#define PENGUIN_CHAR '\U1F427'
+/* if we change the terminal size we want, leave 8 alone and change the second two numbers! (rows/columns respectively */
+#define RESIZE_TERMINAL "\e[8;150;150t"
+#define CLEAR_TERMINAL "\e[2J\e[0;0f"
+
+#endif
