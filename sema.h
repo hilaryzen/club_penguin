add11ebdc5ff7cf4188dbcd7145ea7b73c453277
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-01-04 12:32:18 +0000

    corrected weird name mismatch

--- sema.h
+++ sema.h
@@ -4,7 +4,7 @@
 int sem_config(int key,int flags,int n,...);
 int sem_connect(int key,int n);
 int sem_getval(int semid,int idx);
-void sem_disconnect(int semid);
+void sem_remove(int semid);
 
 void sem_claim(int semid,int idx);
 void sem_release(int semid,int idx);
