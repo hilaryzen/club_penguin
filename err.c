83d2ade13bc6cc1e2706d81dd067aacf00733448
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-01-05 16:13:45 +0000

    instead of exiting, signal self SIGTERM so cleanup is handled

--- err.c
+++ err.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+
+/* void exit_err(int i, char *msg) */
+/* utility function across all methods: if i, a return value from a function, is negative, sends a signal to itself to terminate */
+
 void exit_err(int i,char *msg){
   if( i < 0 ) {
     printf("error #%d during [%s]: %s\n",errno,msg,strerror(errno));
-    exit(errno);
+    kill( getpid(), SIGTERM );
+    // not just exiting because i want the proper exit stuff to happen
   }
 }
